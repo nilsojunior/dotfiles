79829728d2cd33763ec224c9163975c3f0e66a0b
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-02-13 14:03:53 -0300

    Fixing git pull

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-git pull
+git pull --recurse-submodules
 curl -L -o $HOME/dotfiles/stylus/import.json https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json

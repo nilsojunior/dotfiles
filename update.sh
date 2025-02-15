44ef286c026014619e793da03c16d290a459149d
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-02-15 11:31:43 -0300

    New start ssh script

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 
 git pull --recurse-submodules
 curl -L -o $HOME/dotfiles/stylus/import.json https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json
+tldr --update

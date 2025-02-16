613e5d4f612370f3341de84567629d26599fedd4
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-02-16 14:44:47 -0300

    Wallpaper changer with fzf

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@
 git pull --recurse-submodules
 curl -L -o $HOME/dotfiles/stylus/import.json https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json
 tldr --update
+yay -S --needed --noconfirm - < pkglist.lst

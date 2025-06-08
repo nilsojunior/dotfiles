bd2b2362a122b21d110c3c55d9e59cf7b93474fb
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-06-07 23:41:21 -0300

    Cursor script

---
+++ bin/toggle_cursor_on_key_press.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+STATE_FILE="/tmp/hyprland_cursor"
+
+if [ -f "$STATE_FILE" ]; then
+    hyprctl keyword cursor:hide_on_key_press true
+    rm "$STATE_FILE"
+    notify-send "Cursor hide on key press enabled"
+else
+    hyprctl keyword cursor:hide_on_key_press false
+    touch "$STATE_FILE"
+    notify-send "Cursor hide on key press disabled"
+fi

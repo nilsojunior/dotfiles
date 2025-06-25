f88962baedb5d9bd77c482dad221307ad2a43554
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-06-25 17:57:14 -0300

    feat: open projects in neovim

--- scripts/find-dirs.sh
+++ scripts/find-dirs.sh
@@ -18,10 +18,13 @@ all_dirs=("${dirs[@]}" "${extra_dirs[@]}")
 
 all_dirs=$(echo "${all_dirs[@]}" | tr ' ' '\n')
 
-selected=$(echo "${all_dirs}" | sed "s|^$HOME/||" | fzf \
-    --preview "eza -1 --color=always --icons=auto $HOME/{1}" \
-    --preview-window=right:35%)
+selected=$(
+    echo "${all_dirs}" | sed "s|^$HOME/||" | fzf \
+        --preview "eza -1 --color=always --icons=auto $HOME/{}" \
+        --preview-window right:35% \
+        --bind "ctrl-e:execute(nvim $HOME/{})+accept"
+)
 
 if [ -n "$selected" ]; then
-    cd "$HOME/$selected"
+    cd "$HOME/$selected" || exit
 fi

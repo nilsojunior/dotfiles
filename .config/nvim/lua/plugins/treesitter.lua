18999ca0dda008e4428859fc18124ce014bc9081
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-07-15 03:42:11 -0300

    feat: add treesitter-text-objects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,54 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			ensure_installed = {
-				"lua",
-				"python",
-				"c",
-				"cpp",
-				"markdown",
-				"json",
-				"yaml",
-				"toml",
-				"rust",
-				"bash",
-				"css",
-				"java",
-				"go",
-				"typst",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				ensure_installed = {
+					"lua",
+					"python",
+					"c",
+					"cpp",
+					"markdown",
+					"json",
+					"yaml",
+					"toml",
+					"rust",
+					"bash",
+					"css",
+					"java",
+					"go",
+					"typst",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		layz = true,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["ai"] = "@conditional.outer",
+							["ii"] = "@conditional.inner",
+							["al"] = "@loop.inner",
+							["il"] = "@loop.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
 }

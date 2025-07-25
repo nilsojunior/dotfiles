bff9101b2fa5bacea212d128453f0493f38c9514
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-07-25 15:49:12 -0300

    please treesitter do not crash my editor

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		lazy = false,
 		build = ":TSUpdate",
 		config = function()
 			local config = require("nvim-treesitter.configs")
@@ -23,8 +25,20 @@ return {
 					"go",
 					"typst",
 				},
-				highlight = { enable = true },
-				indent = { enable = true },
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							vim.notify("Treesitter: file too big")
+							return true
+						end
+					end,
+				},
+				indent = {
+					enable = true,
+				},
 			})
 		end,
 	},

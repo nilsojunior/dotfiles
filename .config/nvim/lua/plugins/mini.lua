59c2fb5617aabef7413b57a29dc7caf88bf39e35
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-07-27 11:45:30 -0300

    feat: add mini.nvim and remove some plugins

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.splitjoin",
+		version = false,
+		config = function()
+			require("mini.splitjoin").setup()
+		end,
+	},
+}

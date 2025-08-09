23bf0a47907cdbdd8e7e3fd689240c6b05188f9a
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-08-09 09:29:44 -0300

    chore(nvim): update catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,8 +4,12 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
+			flavour = "mocha",
 			transparent_background = true,
 			no_italic = true,
+			float = {
+				transparent = true,
+			},
 		})
 		vim.cmd.colorscheme("catppuccin")
 	end,

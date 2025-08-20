e03221053f1225f551234723c960ba2354e148b7
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-08-20 20:45:29 -0300

    feat: add install

--- manager.sh
+++ manager.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CONFIG=".config"
-HOME_D="home"
-
 sync() {
+    CONFIG=".config"
+    HOME_D="home"
+
     for file in $(find "$CONFIG" -maxdepth 1 -not -path "$CONFIG" | sed "s|$CONFIG/||"); do
         source="$PWD/$CONFIG/$file"
         target="$HOME/$CONFIG/$file"
@@ -32,4 +32,49 @@ sync() {
     fi
 }
 
-sync
+install() {
+    paru="$HOME/repos/paru"
+    cwd=$(pwd)
+    sudo pacman -S --needed --noconfirm git base-devel
+    git clone https://aur.archlinux.org/paru.git "$paru"
+    cd "$HOME/paru" || exit
+    makepkg -si --noconfirm
+    cd "$cwd" || exit
+
+    paru -S --needed --noconfirm - <pkglist.lst
+
+    host=$(hostname)
+    if [ "$host" = "archbtw" ]; then
+        paru -S --needed --noconfirm nvidia nvidia-utils lib32-nvidia-utils egl-wayland
+    elif [ "$host" = "arch" ]; then
+        echo "Skipping Nvidia drives..."
+    else
+        echo "Hostname: '$host' is not expected."
+    fi
+
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+
+    sync
+
+    bat cache --build
+
+    # Cursor setup
+    echo "[Icon Theme]" >>"/usr/share/icons/default/index.theme"
+    echo "Inherits=Bibata-Modern-Ice" >>"/usr/share/icons/default/index.theme"
+
+    # Spicetify setup
+    sudo chmod a+wr /opt/spotify
+    sudo chmod a+wr /opt/spotify/Apps -R
+
+    # Catppuccin gtk theme
+    ./install.py mocha pink
+
+    flatpak install -y dev.vencord.Vesktop
+    flatpak install -y com.stremio.Stremio
+
+    chsh -s "$(which zsh)"
+
+    # Use dash as system shell
+    sudo ln -sfT dash /usr/bin/sh
+    sudo cp hooks/bash-update.hook /usr/share/libalpm/
+}

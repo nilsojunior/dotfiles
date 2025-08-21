5905ff6789081e746276409589766b6dd28f0d11
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-08-21 13:21:33 -0300

    feat: add more setups

--- manager.sh
+++ manager.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sync() {
+setup_symlinks() {
     CONFIG=".config"
     HOME_D="home"
 
@@ -32,18 +32,51 @@ sync() {
     fi
 }
 
-install() {
+setup_kanata() {
+    sudo groupadd uinput
+
+    sudo usermod -aG input "$USER"
+    sudo usermod -aG uinput "$USER"
+
+    echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee "/etc/udev/rules.d/99-input.rules"
+
+    sudo udevadm control --reload-rules && sudo udevadm trigger
+}
+
+setup_spicetify() {
+    sudo chmod a+wr /opt/spotify
+    sudo chmod a+wr /opt/spotify/Apps -R
+}
+
+setup_dash() {
+    sudo ln -sfT dash /usr/bin/sh
+    sudo cp hooks/bash-update.hook /usr/share/libalpm/
+}
+
+setup_cursor() {
+    echo "[Icon Theme]" >>"/usr/share/icons/default/index.theme"
+    echo "Inherits=Bibata-Modern-Ice" >>"/usr/share/icons/default/index.theme"
+}
+
+install_pkgs() {
+    paru -S --needed --noconfirm - <pkglist.lst
+}
+
+install_paru() {
     paru="$HOME/repos/paru"
     cwd=$(pwd)
+
     sudo pacman -S --needed --noconfirm git base-devel
     git clone https://aur.archlinux.org/paru.git "$paru"
+
     cd "$HOME/paru" || exit
     makepkg -si --noconfirm
     cd "$cwd" || exit
+}
 
-    paru -S --needed --noconfirm - <pkglist.lst
-
+install_machine_specific_pkgs() {
     host=$(hostname)
+
     if [ "$host" = "archbtw" ]; then
         paru -S --needed --noconfirm nvidia nvidia-utils lib32-nvidia-utils egl-wayland
     elif [ "$host" = "arch" ]; then
@@ -51,20 +84,23 @@ install() {
     else
         echo "Hostname: '$host' is not expected."
     fi
+}
+
+install() {
+    install_paru
+    install_pkgs
+    install_machine_specific_pkgs
 
     git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 
-    sync
+    setup_symlinks
 
     bat cache --build
 
-    # Cursor setup
-    echo "[Icon Theme]" >>"/usr/share/icons/default/index.theme"
-    echo "Inherits=Bibata-Modern-Ice" >>"/usr/share/icons/default/index.theme"
-
-    # Spicetify setup
-    sudo chmod a+wr /opt/spotify
-    sudo chmod a+wr /opt/spotify/Apps -R
+    setup_spicetify
+    setup_dash
+    setup_cursor
+    # setup_kanata
 
     # Catppuccin gtk theme
     ./install.py mocha pink
@@ -73,8 +109,12 @@ install() {
     flatpak install -y com.stremio.Stremio
 
     chsh -s "$(which zsh)"
+}
 
-    # Use dash as system shell
-    sudo ln -sfT dash /usr/bin/sh
-    sudo cp hooks/bash-update.hook /usr/share/libalpm/
+update() {
+    git pull --recurse-submodules
+    git submodule update --remote
+    curl -sL -o "$HOME/dotfiles/stylus/import.json" https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json
+    tldr --update
+    install_pkgs
 }

87d353997c1ef40ec95ec31b769032e5e8a3e633
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-08-22 17:10:19 -0300

    feat: remove stow and unused scripts

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git pull --recurse-submodules
-git submodule update --remote
-curl -sL -o "$HOME/dotfiles/stylus/import.json" https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json
-tldr --update
-killall hyprpaper
-hyprpaper

0582f8a0c99ebc80065fb5d3601a2317f20299fb
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-08-28 23:05:46 -0300

    feat(Firefox): add user.js

---
+++ firefox/user.js
@@ -0,0 +1,8 @@
+// Custom CSS
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// File picker
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+
+// Remove search engine icon from search bar
+user_pref("browser.urlbar.scotchBonnet.enableOverride", false);

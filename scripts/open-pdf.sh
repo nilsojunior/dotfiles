e6e928354c4bbc204da55c3f2f357ab28d82bb46
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-08-30 06:08:17 -0300

    feat: open-pdf.sh does not need a tmux window anymore

--- scripts/open-pdf.sh
+++ scripts/open-pdf.sh
@@ -6,8 +6,7 @@ selected=$(
         fzf
 )
 
-selected_name=$(basename "$selected" | tr . _)
-
 if [ "$selected" ]; then
-    tmux new-window -n "$selected_name" -d xdg-open "$HOME/$selected"
+    setsid zathura "$HOME/$selected" &
+    tmux kill-window
 fi

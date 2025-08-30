fd4a2db4c319ccddfe32cac932653e353b9c10d1
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-08-30 06:24:20 -0300

    chore(nvim): remove mini.surround

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,12 +1,5 @@
 return {
 	{
-		"echasnovski/mini.surround",
-		version = false,
-		config = function()
-			require("mini.surround").setup()
-		end,
-	},
-	{
 		"echasnovski/mini.splitjoin",
 		version = false,
 		config = function()

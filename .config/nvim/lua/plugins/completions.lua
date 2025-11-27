be866a87f167043dffc47952b72596084a522e30
Nilso Júnior <162613094+Nilsojunior@users.noreply.github.com>
2025-11-27 09:42:26 -0300

    feat(nvim): add completions for vim-dadbod

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -153,6 +153,12 @@ return {
 					{ name = "path" },
 				}),
 			})
+			cmp.setup.filetype({ "sql" }, {
+				sources = {
+					{ name = "vim-dadbod-completion" },
+					{ name = "buffer" },
+				},
+			})
 		end,
 	},
 }

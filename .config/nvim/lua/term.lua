a8799f49208d88d48e6fa90124c2b817ee6b214f
Nilso Júnior <162613094+Nilsojunior@users.noreply.github.com>
2025-12-01 08:38:36 -0300

    fix(nvim): create keymap in the right scope in term.lua

--- .config/nvim/lua/term.lua
+++ .config/nvim/lua/term.lua
@@ -4,6 +4,14 @@ local buf
 
 local keymap = vim.keymap.set
 
+local function run_cmd()
+	if not cmd or cmd:match("^%s*$") then
+		vim.notify("There is no command to run", vim.log.levels.WARN)
+		return
+	end
+	vim.fn.system("tmux.sh '" .. cmd .. "'")
+end
+
 local function create_win()
 	if buf then
 		vim.api.nvim_buf_attach(buf, false, {})
@@ -19,22 +27,14 @@ local function create_win()
 	vim.api.nvim_set_option_value("relativenumber", false, { win = win })
 	vim.api.nvim_set_option_value("filetype", "sh", { buf = buf })
 	vim.api.nvim_feedkeys("A", "n", false)
-end
 
-local function run_cmd()
-	if not cmd or cmd:match("^%s*$") then
-		vim.notify("There is no command to run", vim.log.levels.WARN)
-		return
-	end
-	vim.fn.system("tmux.sh '" .. cmd .. "'")
+	keymap({ "i", "n" }, "<CR>", function()
+		cmd = vim.api.nvim_get_current_line()
+		run_cmd()
+		vim.api.nvim_win_close(win, false)
+		vim.api.nvim_command("stopinsert")
+	end, { buffer = buf, noremap = true, silent = true })
 end
 
 keymap("n", "<leader>re", create_win)
 keymap("n", "<leader>rw", run_cmd, {})
-
-keymap({ "i", "n" }, "<CR>", function()
-	cmd = vim.api.nvim_get_current_line()
-	run_cmd()
-	vim.api.nvim_win_close(win, false)
-	vim.api.nvim_command("stopinsert")
-end, { buffer = buf })

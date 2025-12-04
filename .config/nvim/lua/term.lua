96ed9ea9b95276b565e06e84b1903e245a8c9321
Nilso Júnior <162613094+Nilsojunior@users.noreply.github.com>
2025-12-04 16:51:27 -0300

    feat: run command with neovim terminal

--- .config/nvim/lua/term.lua
+++ .config/nvim/lua/term.lua
@@ -1,6 +1,4 @@
 local cmd
-local win
-local buf
 
 local keymap = vim.keymap.set
 
@@ -9,32 +7,55 @@ local function run_cmd()
 		vim.notify("There is no command to run", vim.log.levels.WARN)
 		return
 	end
-	vim.fn.system("tmux.sh '" .. cmd .. "'")
+
+	local buf = vim.api.nvim_create_buf(true, true)
+	vim.api.nvim_open_win(buf, true, {
+		split = "below",
+	})
+
+	local shell_cmd = string.format("tmux.sh '%s'", cmd)
+	vim.fn.jobstart(shell_cmd, {
+		term = true,
+		on_stdout = function()
+			vim.cmd("startinsert")
+		end,
+		-- Avoid the process exited message
+		on_exit = function()
+			vim.api.nvim_buf_delete(buf, {})
+		end,
+	})
+
+	keymap("t", "<ESC>", "<C-\\><C-n>", {
+		buffer = buf,
+		noremap = true,
+		silent = true,
+	})
 end
 
 local function create_win()
-	if buf then
-		vim.api.nvim_buf_attach(buf, false, {})
-	else
-		buf = vim.api.nvim_create_buf(true, true)
-	end
+	local buf = vim.api.nvim_create_buf(true, true)
 
-	win = vim.api.nvim_open_win(buf, true, {
+	local win = vim.api.nvim_open_win(buf, true, {
 		split = "below",
 		height = 1,
 	})
 	vim.api.nvim_set_option_value("number", false, { win = win })
 	vim.api.nvim_set_option_value("relativenumber", false, { win = win })
 	vim.api.nvim_set_option_value("filetype", "sh", { buf = buf })
-	vim.api.nvim_feedkeys("A", "n", false)
+
+	vim.cmd("startinsert")
 
 	keymap({ "i", "n" }, "<CR>", function()
 		cmd = vim.api.nvim_get_current_line()
 		run_cmd()
 		vim.api.nvim_win_close(win, false)
-		vim.api.nvim_command("stopinsert")
-	end, { buffer = buf, noremap = true, silent = true })
+		vim.cmd("stopinsert")
+	end, {
+		buffer = buf,
+		noremap = true,
+		silent = true,
+	})
 end
 
 keymap("n", "<leader>re", create_win)
-keymap("n", "<leader>rw", run_cmd, {})
+keymap("n", "<leader>rw", run_cmd)
